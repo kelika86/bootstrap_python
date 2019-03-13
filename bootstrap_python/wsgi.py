c7fd25f0ffc6c175a9ae674d88f12ce584fc0e0c
keikokamiya <cherry.blossoms86@hotmail.com>
2019-03-13 14:11:36 -0700

    post-deployment_changes

--- bootstrap_python/wsgi.py
+++ bootstrap_python/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for climate_change project.
+WSGI config for bootstrap_python project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'climate_change.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bootstrap_python.settings')
 
 application = get_wsgi_application()
