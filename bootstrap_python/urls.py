fd3cc07a5088865cae1ef328aa85dbfb528b4361
keikokamiya <cherry.blossoms86@hotmail.com>
2019-03-21 13:40:59 -0700

    added bz

--- bootstrap_python/urls.py
+++ bootstrap_python/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url, include
+from django.urls import url, include
 
 urlpatterns = [
     url(r'^', include('apps.first_app.urls')),
